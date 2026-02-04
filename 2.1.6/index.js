15007e6c11e7e4b8db6ca2623e39f73fb027e4c9
Your Name <you@example.com>
2026-02-04 12:06:34 +0500

    .

---
+++ 2.1.6/index.js
@@ -0,0 +1,15 @@
+// console.log(4 * parseFloat(prompt("Perimetr...")));
+
+// console.log(Math.pow(parseFloat(prompt("Yuza...")), 2));
+
+// let a = parseFloat(prompt("a...")), b = parseFloat(prompt("b..."))
+// console.log("Yuzasi = "+ a*b, "Perimetri = "+ 2*(a+b));
+
+// console.log(Math.PI * parseFloat(prompt("Uzunli...")))
+
+// let a = parseFloat(prompt("Yon tomon..."))
+// console.log("Hajmi = "+ Math.pow(a, 3), "To'la sirti = "+ 6 * Math.pow(a, 2))
+
+// let a = +(prompt("a...")), b = +(prompt("b...")), c = +(prompt("c..."));
+// console.log("Hajmi = "+ a*b*c, "Sirti = "+ 2*(a*b+b*c+a*c));
+
