e0bbd580e6999ea0666e187fd360054428eb0568
Your Name <you@example.com>
2026-02-04 23:11:20 +0500

    .

--- 2.1.6/index.js
+++ 2.1.6/index.js
@@ -3,7 +3,7 @@
 // console.log(Math.pow(parseFloat(prompt("Yuza...")), 2));
 
 // let a = parseFloat(prompt("a...")), b = parseFloat(prompt("b..."))
-// console.log("Yuzasi = "+ a*b, "Perimetri = "+ 2*(a+b));
+// console.log("Yuzasi = "+ (a*b), "Perimetri = "+ (2*(a+b)));
 
 // console.log(Math.PI * parseFloat(prompt("Uzunli...")))
 
@@ -12,4 +12,70 @@
 
 // let a = +(prompt("a...")), b = +(prompt("b...")), c = +(prompt("c..."));
 // console.log("Hajmi = "+ a*b*c, "Sirti = "+ 2*(a*b+b*c+a*c));
+
+// let R = +(prompt("R..."));
+// console.log("Uzunligi = "+ (2*Math.PI*R), "Yuzasi = "+ (Math.PI*Math.pow(R, 2)));
+
+// let a = +(prompt("a...")), b = +(prompt("b..."));
+// console.log("O'rta arifmetika = "+ (a+b)/2)
+
+// let a = +(prompt("a...")), b = +(prompt("b..."));
+// console.log("O'rta geometrika = "+ Math.sqrt(a*b));
+
+// let a = +(prompt("a...")), b = +(prompt("b..."));
+// console.log("Yig'indisi = "+ (a+b), "Ko'paytmasi = "+ (a*b), "a^2 ="+ Math.pow(a, 2), "b^2 ="+ Math.pow(b, 2))
+
+// let a = +(prompt("a...")), b = +(prompt("b..."));
+// console.log("Yig'indisi = "+ (a+b), "Ko'paytmasi = "+ (a*b), "|a| ="+ Math.abs(a), "|b| ="+ Math.abs(b))
+
+// let a = +(prompt("a...")), b = +(prompt("b...")), c = (Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)))
+// console.log("Gipotenuza = "+ c, "Perimetri = "+ (a+b+c));
+
+// let R1 = +(prompt("R1...")), R2 = +(prompt("R2..."));
+// console.log("S1 = "+ (Math.PI * R1), "S2 = "+ (Math.PI * R2), "S3 = "+ (Math.PI * (R1-R2)), "R1 > R2 -> "+ (R1>R2));
+
+// let L = +(prompt("L...")), R = L/(2*Math.PI)
+// console.log("R = "+ (R), "S = "+ (Math.PI * Math.pow(R, 2)))
+
+// let S = +(prompt("S...")), R = Math.sqrt(S/Math.PI);
+// console.log("R = "+ (R), "d = "+ (2 * R));
+
+// console.log(Math.abs(parseFloat(prompt("x2...")) - parseFloat(prompt("x1..."))))
+
+// let A = +(prompt("A...")), B = +(prompt("B...")), C = +(prompt("C..."));
+// let AC = Math.abs(C - A), BC = Math.abs(B - C)
+// console.log("AC = "+ (AC), "BC = "+ (BC), "SUM = "+ (AC + BC))
+
+// let A = +(prompt("A...")), B = +(prompt("B...")), C = +(prompt("C..."));
+// let AC = Math.abs(C - A), BC = Math.abs(B - C);
+// console.log("AC = "+ (AC), "BC = "+ (BC), "product = "+ (AC * BC));
+
+// const X1 = 2, Y1 = 3, X2 = 12, Y2 = 8, X3 = 24, Y3 = 16;
+// const width = Math.abs(X2 - X1), height = Math.abs(Y2 - Y1);
+// console.log("perimetr = "+ (2 * (width + height)), "yuza = "+ (width * height));
+// console.log("√(x2-x1)^2+(y2-y1)^2 = " + (Math.sqrt(Math.pow(X2-X1, 2) + Math.pow(Y2-Y1, 2))));
+// const a = Math.sqrt(Math.pow(X3 - X2, 2) + Math.pow(Y3 - Y2, 2));
+// const b = Math.sqrt(Math.pow(X3 - X1, 2) + Math.pow(Y3 - Y1, 2));
+// const c = Math.sqrt(Math.pow(X2 - X1, 2) + Math.pow(Y2 - Y1, 2));
+// const p = (a + b + c) / 2, S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+// console.log("S = "+ S, "p = "+ p)
+
+// let a = true, b = "noob", c = null;
+// console.log("a = "+ a, "b = "+ b, "c = "+ c)
+// x = a, a = b, b = x;
+// console.log("a = "+ a, "b = "+ b, "c = "+ c)
+// x = c, c = b, b = a, a = x;
+// console.log("a = "+ a, "b = "+ b, "c = "+ c)
+// x = a, a = b, b = c, c = x;
+// console.log("a = "+ a, "b = "+ b, "c = "+ c)
+
+// const x = 2;
+// let y = 3 * Math.pow(x, 6) - 6 * Math.pow(x, 2) - 7;
+// console.log("y = "+ y);
+// y = 4 * Math.pow(x-3, 6) - 7 * Math.pow(x-3, 2) + 2;
+// console.log("y = "+ y);
+
+// const a = +(prompt("A..."));
+// console.log("A^2, A^4, A8 ->", Math.pow(a,2), Math.pow(a,4), Math.pow(a,8));
+// console.log("A^2, A^3, A5, A^10, A^15 ->", Math.pow(a,2), Math.pow(a,3), Math.pow(a,5), Math.pow(a,10), Math.pow(a,15));
 
